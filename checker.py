b0ebfc16bccf7335d6ee80296bc35071062f266d
DragonScraper <mazinosmail@gmail.com>
2024-06-13 02:03:21 +0100

    add support for sites without api add two new sites add logging to console & file add gitignore file some bugs fixing and improvments

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 import aiohttp
 import asyncio
+import logging
 import json
 import aiofiles
 import pycountry
@@ -7,6 +8,8 @@ from itertools import islice
 from functools import reduce
 from asyncio import TimeoutError
 
+
+logger =  logging.getLogger('checker')
 
 data_p =  "proxies.json"
 
@@ -26,32 +29,47 @@ async def add_value_to_json(file_path=data_p, country = None ,
         await f.write(json.dumps(DATA,indent=4))
 
 def json_path(data , keys :  list):
-    return reduce(lambda d, key: d[key], keys, data)
+    try:return reduce(lambda d, key: d[key], keys, data)
+    except:return None
+
+def soup_path(soup , selector , text):
+    data =  soup.select_one(selector)
+    if text:
+        return data.get_text(strip=True) if data else None
+    else:
+        return data
+
+def content_path(data ,  key ,  api , text  =  True ):
+    return json_path(data,key) if api  ==  True else soup_path(data,key,text)
 
 def country(co):
     try:return pycountry.countries.search_fuzzy(co)[0].alpha_3
-    except :return "NONE"
+    except :
+        logger.warning(f'co country in {co}')
+        return "NONE"
 
 async def site_params(proxy , site_pa):
-    return  (json_path(proxy,site_pa["l_proxy"]) ,
-             json_path(proxy,site_pa["protocol"]),
-             country(json_path(proxy,site_pa["country"]))
-             ) if site_pa["api"] ==  True else (
-                 
-             )
-
+    return ( 
+            "%s://%s:%s" % (content_path(proxy,site_pa["protocol"],site_pa["api"]) or "http",
+            content_path(proxy,site_pa["ip"],site_pa["api"]),
+            content_path(proxy,site_pa["port"],site_pa["api"]) ),
+            content_path(proxy,site_pa["protocol"],site_pa["api"]) or "http",
+            country(content_path(proxy,site_pa["country"],site_pa["api"]))
+    )
 
 async def check_proxy(proxy , site_pa):
     l_proxy , protocol , country = await site_params(proxy,site_pa)
+    logger.debug(f"[proxy : {l_proxy}] [ protocol : {protocol}]  [country :  {country}]")
     url = 'http://httpbin.org/ip'  
     connector = aiohttp.TCPConnector(limit=None) 
     async with aiohttp.ClientSession(connector=connector) as session:
         try:
             async with session.get(url,headers={},proxy=l_proxy,timeout=3) as response:
                 if response.status == 200:
+                    logger.debug(f'added : {l_proxy}')
                     await add_value_to_json(country=country,protocol=protocol,value=l_proxy)
                 else:
-                    print(f"Failed to connect through proxy. Status code: {response.status}")
+                    logger.warning(f"Failed to connect through {l_proxy}. Status code: {response.status}")
         except TimeoutError as e:
             pass
 
@@ -88,6 +106,6 @@ async def print_when_done(tasks):
 
 
 async def  checker(site , data):
-    proxies = json_path(data,site['proxies']) if site['api'] == True else None
+    proxies =content_path(data,site['proxies'],site['api'],text=False)
     await print_when_done((check_proxy(proxy,site)  for proxy in proxies))
 
--- main.py
+++ main.py
@@ -1,15 +1,48 @@
 import aiohttp
 import asyncio
+import logging
 import json
+
+from bs4 import BeautifulSoup 
+
 from checker import  checker
 
-async def fetch_json(session, site,headers):
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  
+    handlers=[
+        logging.FileHandler("logs.log"), 
+        logging.StreamHandler() 
+    ]
+)
+
+
+logger =  logging.getLogger('main')
+
+
+
+async def fetch_json(session, site):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0',
+                'Referer': site["site"]
+                }
+    
     async with session.get(site['url'],headers =  headers) as response:
+        logger.info(f"{site['url']} collcted proxies")
         return  {"site":site,"res": await response.json()}
     
-async def fetch_soup(session, url,headers):
-    async with session.get(url,headers =  headers) as response:
-        return await response.text()
+async def fetch_soup(session, site):
+
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0',
+                'Referer': site["site"]
+                }
+    
+    async with session.get(site["url"],headers =  headers) as response:
+        logger.info(f"{site['url']} collcted proxies")
+        return {"site":site,"res": BeautifulSoup( await response.text() , "html.parser")}
+        
 
 
 def get_sites():
@@ -18,15 +51,12 @@ def get_sites():
 async def fetch_all():
 
     sites  = get_sites()
-
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0',
-                                            'Referer': "refer",}
     async with aiohttp.ClientSession() as session:
         tasks = []
         for site in sites:
-            tasks.append(fetch_json(session, site  ,headers=headers)
+            tasks.append(fetch_json(session, site)
                         if site['api'] == True  
-                        else fetch_soup(session,site['url'],headers=headers) )
+                        else fetch_soup(session,site) )
             
         responses = await asyncio.gather(*tasks)
         return responses
@@ -35,6 +65,8 @@ async def fetch_all():
 
 
 async  def main():
+
+    logger.info('< executing PRPXYHARVEST >')
 
     resulst =  await fetch_all()
 
